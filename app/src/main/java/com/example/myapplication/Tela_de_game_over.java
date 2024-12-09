56fd4682d0ab4b5a939ba1dcfaa959336cc3b604
LuisAlveser <ericssonluis@gmail.com>
2024-12-09 10:29:20 -0300

    Adição da tela de fim de jogo e alterações no layout dos  componentes

--- app/src/main/java/com/example/myapplication/Tela_de_game_over.java
+++ app/src/main/java/com/example/myapplication/Tela_de_game_over.java
@@ -10,39 +10,28 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class Tela_de_game_over extends AppCompatActivity {
+    public void Voltar_menu(View v){
+        Intent i =new Intent(this, MainActivity.class);
+        startActivity(i);
 
-
-
-
-    //Função para ir para a tela de jogo
-    public void Jogar(View v){
+    }
+    public void JOGAR_Novamente(View v){
         Intent i =new Intent(this, Jogar.class);
         startActivity(i);
 
     }
 
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
-
-
-
-
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_tela_de_game_over);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-
         });
-
-
     }
 }
